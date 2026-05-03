23fec2d7c2961e3aa3d7c9d8a3d7df80d541f7d4
Shriyans <105914805@student.swin.edu.au>
2026-05-03 21:02:48 +1000

    added tables no links just tables

--- Database.sql
+++ Database.sql
@@ -5,5 +5,96 @@
 -- HOANG SECTION --
 hehe
 -- SHRIYANS SECTION --
-six sevennnn
+CREATE TABLE Archer (
+    ArcherID int PRIMARY KEY,
+    firstName varChar,
+    lastName varChar,
+    Gender VarChar,
+    DOB date,
+    DefaultEquipmentID int,
+    ClubID int
+);
+
+CREATE TABLE EquipmentType (
+    EquipmentID int PRIMARY KEY,
+    Name varChar,
+    DivisionCode varChar
+);
+
+CREATE TABLE Club (
+    ClubID int PRIMARY KEY,
+    Name varChar
+);
+
+CREATE TABLE BaseRound (
+    BaseRoundID int PRIMARY KEY,
+    RoundName varChar
+);
+
+CREATE TABLE JunctionRoundRange (
+    BaseRoundID int,
+    RangeID int,
+    RangePosition int
+);
+
+CREATE TABLE Range (
+    RangeID int PRIMARY KEY,
+    DistanceToTargetM int,
+    TargetFaceCm int,
+    NumberOfEnds int
+);
+
+CREATE TABLE Class (
+    ClassID int PRIMARY KEY,
+    Gender varChar,
+    MinAge int,
+    MaxAge int
+);
+
+CREATE TABLE EquivalentRound (
+    EquivalentRoundID int PRIMARY KEY,
+    BaseRoundID int,
+    ActualRoundID int,
+    ClassID int,
+    EquipmentID varChar,
+    ValidFrom date,
+    ValidTo date
+);
+
+CREATE TABLE Competition (
+    CompetitionID int PRIMARY KEY,
+    BaseRoundID int,
+    ClubID int,
+    ChampionshipID int,
+    CompetitionDate date,
+    CompetitionName varChar
+);
+
+CREATE TABLE Championship (
+    ChampionshipID int PRIMARY KEY,
+    ChampionshipName varChar,
+    Year int
+);
+
+CREATE TABLE RoundScore (
+    ScoreID int PRIMARY KEY,
+    CompetitionID int,
+    ArcherID int,
+    BaseRoundID int,
+    IsApproved bool,
+    Date date,
+    Time time
+);
+
+CREATE TABLE End (
+    EndID int PRIMARY KEY,
+    ScoreID int,
+    Position int
+);
+
+CREATE TABLE Arrow (
+    ArrowID int PRIMARY KEY,
+    EndID int,
+    Score int
+);
 -- EUGENE SECTION -- 

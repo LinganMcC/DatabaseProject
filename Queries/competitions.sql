1680f76f4c5cd9a28daa8774d2db4dbea39b86b9
eugene719 <eugene7192113@gmail.com>
2026-05-16 03:34:47 +1000

    competitions sql to match readme naming convention

--- Queries/competitions.sql
+++ Queries/competitions.sql
@@ -6,20 +6,20 @@ select
     cmp.CompetitionName,
     cl.Name as ClubName,
     br.RoundName,
-    arc.FirstName,
-    arc.LastName,
+    a.FirstName,
+    a.LastName,
     cmp.CompetitionDate,
-    sum(a.Score) as TotalArrowScore,
-    count(a.ArrowID) as TotalArrows
+    sum(ar.Score) as TotalArrowScore,
+    count(ar.ArrowID) as TotalArrows
 from Competition cmp
 join RoundScore rs on cmp.CompetitionID = rs.CompetitionID
-join Archer arc on rs.ArcherID = arc.ArcherID
+join Archer a on rs.ArcherID = a.ArcherID
 join End e on rs.ScoreID = e.ScoreID
-join Arrow a on e.EndID = a.EndID
+join Arrow ar on e.EndID = ar.EndID
 join Club cl on cmp.ClubID = cl.ClubID
 join BaseRound br on rs.BaseRoundID = br.BaseRoundID
 where rs.IsApproved = 1
-group by cmp.CompetitionID, arc.ArcherID, cl.ClubID, br.BaseRoundID, cmp.CompetitionDate
+group by cmp.CompetitionID, a.ArcherID, cl.ClubID, br.BaseRoundID, cmp.CompetitionDate
 
 -- View yearly club championship results and identify winners.
 -- ------------------------------------------------------------------------------------------------

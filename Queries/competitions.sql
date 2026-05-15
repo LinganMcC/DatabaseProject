d1dd27a5c62e0800bdd216e909926444000fbda1
eugene719 <eugene7192113@gmail.com>
2026-05-15 19:51:30 +1000

    progress on competition results query

--- Queries/competitions.sql
+++ Queries/competitions.sql
@@ -3,8 +3,24 @@
 -- Description...
 -- Technical description...
 select
-from competition c
-join roundscore rs on c.id = rs.competitionid
+    cmp.CompetitionName,
+    cl.Name as ClubName,
+    br.RoundName,
+    arc.FirstName,
+    arc.LastName,
+    cmp.CompetitionDate,
+    sum(a.Score) as TotalArrowScore,
+    count(a.ArrowID) as TotalArrows
+from Competition cmp
+join RoundScore rs on cmp.CompetitionID = rs.CompetitionID
+join Archer arc on rs.ArcherID = arc.ArcherID
+join End e on rs.ScoreID = e.ScoreID
+join Arrow a on e.EndID = a.EndID
+join Club cl on cmp.ClubID = cl.ClubID
+join BaseRound br on rs.BaseRoundID = br.BaseRoundID
+where rs.IsApproved = 1
+group by cmp.CompetitionID, arc.ArcherID, cl.ClubID, br.BaseRoundID, cmp.CompetitionDate
+
 -- View yearly club championship results and identify winners.
 -- ------------------------------------------------------------------------------------------------
 -- Description...
